6d085b5b62c2ceda5b7009aed1b18f8ed7f3285d
MariCeo19 <marianathayza3@gmail.com>
2023-04-27 17:17:58 -0300

    atualizaçoes 27/04/2023

--- app/src/main/java/com/example/guthy/MainActivity.kt
+++ app/src/main/java/com/example/guthy/MainActivity.kt
@@ -3,12 +3,19 @@ package com.example.guthy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.guthy.ui.theme.GuthyTheme
 
@@ -17,30 +24,42 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GuthyTheme {
-                // A surface container using the 'background' color from the theme
+             // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    DadoSky()
+                }
                 }
             }
         }
-    }
 }
 
+@Preview
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun DadoSky(){
+    var resultado = 1
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     )
-}
+    {
+        Image(
+            painter = painterResource(id =R.drawable.dice_1 ) ,
+            contentDescription = null
+        )
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GuthyTheme {
-        Greeting("Android")
+    Button(
+    onClick = {
+        resultado = ( 1..6).random()
     }
+) {
+        Text(
+            text = "Rodar"
+        )
+ }
+  }
 }
